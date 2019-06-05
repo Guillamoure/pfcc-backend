ec4884ba6801a0ad7226b2ef1844cda3d4b945e0
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-06-05 16:59:49 -0400

    create class feature created

--- app/controllers/api/v1/klass_features_controller.rb
+++ app/controllers/api/v1/klass_features_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::KlassFeaturesController < ApplicationController
-  
+
+  def create
+    @klass_feature = KlassFeature.create(name: params["features"]["name"], klass_id: params["klass_id"], level_learned: params["features"]["level_learned"], description: params["features"]["description"])
+    if @klass_feature.valid?
+      
+      render json: { klass_feature: KlassFeatureSerializer.new(@klass_feature) }, status: 200
+    else
+      render json: {error: "Could not create" }, status: 401
+    end
+  end
 
 end
