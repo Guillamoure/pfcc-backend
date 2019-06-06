6008b0301a47a31488f7c793b474bdf80c8b1568
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-06-06 11:40:37 -0400

    class features update controller, and some class feature validation

--- app/models/klass_feature.rb
+++ app/models/klass_feature.rb
@@ -1,3 +1,12 @@
 class KlassFeature < ApplicationRecord
   belongs_to :klass
+  validates :name, presence: true
+  validates :description, presence: true
+  validate :correct_level_range
+
+  def correct_level_range
+    if level_learned < 1 || level_learned > 20
+      errors.add(:level_learned, "must be between 1 and 20")
+    end
+  end
 end
