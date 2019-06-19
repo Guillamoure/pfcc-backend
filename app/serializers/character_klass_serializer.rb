2b0100240662c1e9ea832dd692322bd5e5524ecc
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-06-19 15:37:32 -0400

    changed serializer to allow specific data to appear at during fetches

--- app/serializers/character_klass_serializer.rb
+++ app/serializers/character_klass_serializer.rb
@@ -1,5 +1,5 @@
 class CharacterKlassSerializer < ActiveModel::Serializer
-  attributes :id, :character_id, :klass_id, :level
+  attributes :id, :character_id, :klass_id, :level, :hp
 
   belongs_to :character
   belongs_to :klass
