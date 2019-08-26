936833a598b83bf2fd3fb3279d2360adc6f2c0dd
Jack Hilscher <Jack@lightturtle.local>
2019-08-25 23:36:02 -0400

    added lethal and non lethal damage to character

---
+++ db/migrate/20190825184433_add_damage_to_characters.rb
@@ -0,0 +1,6 @@
+class AddDamageToCharacters < ActiveRecord::Migration[5.2]
+  def change
+    add_column :characters, :lethal_damage, :integer, default: 0
+    add_column :characters, :non_lethal_damage, :integer, default: 0
+  end
+end
