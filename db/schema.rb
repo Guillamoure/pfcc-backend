57029dade4b94a261160a78c4a27aecc478603b4
Jack <jhilscheriv@gmail.com>
2019-09-03 15:14:56 -0400

    added spells per day as spellcasting, and added two class features to oracle, just for ease of reading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_184433) do
+ActiveRecord::Schema.define(version: 2019_08_28_140408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,13 @@ ActiveRecord::Schema.define(version: 2019_08_25_184433) do
 
   create_table "skillsets", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "spellcastings", force: :cascade do |t|
+    t.string "ability_score"
+    t.integer "klass_feature_id"
+    t.boolean "prepared"
+    t.boolean "limited"
   end
 
   create_table "users", force: :cascade do |t|
