1411b93d5574be40c13f82526f1c51ff9a1f9c2d
Jack Hilscher <Jack@lightturtle.local>
2019-11-25 21:58:46 -0500

    added spells, updated klass and features in serializers, spell and component serializer

--- app/controllers/api/v1/spells_controller.rb
+++ app/controllers/api/v1/spells_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::SpellsController < ApplicationController
 
   def index
-    @spells = Spell.all
+    spells = Spell.all
+    @spells = spells.map do |sp|
+      SpellSerializer.new(sp)
+    end
     render json: @spells, status: 200
   end
 
--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,14 +1,20 @@
 class CharacterSerializer < ActiveModel::Serializer
-  attributes :id, :name, :alignment, :deity, :homeland, :gender, :age, :height, :weight, :hair, :eyes, :user_id, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :description, :background, :any_bonus, :character_skillset_skills, :lethal_damage, :non_lethal_damage, :temp_hp
+  attributes :id, :name, :alignment, :deity, :homeland, :gender, :age, :height, :weight, :hair, :eyes, :user_id, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :description, :background, :any_bonus, :character_skillset_skills, :lethal_damage, :non_lethal_damage, :temp_hp, :uniq_klasses
 
   belongs_to :user
   has_many :character_klasses
-  has_many :klasses, through: :character_klasses
-  has_many :klass_features, through: :klasses
+  # has_many :klasses, through: :character_klasses
+  # has_many :klass_features, through: :klasses
   belongs_to :race
   belongs_to :skillset
   has_many :character_skillset_skills
   has_many :cast_spells
   has_many :known_spells
   has_many :prepared_spells
+
+  def uniq_klasses
+    self.object.klasses.uniq.map do |kl|
+      KlassSerializer.new(kl)
+    end
+  end
 end
