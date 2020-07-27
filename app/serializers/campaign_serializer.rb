7c7663d28b5a8fe65ad1d48a6c3cf64305367868
Guillamoure <jhilscheriv@gmail.com>
2020-07-27 10:04:12 -0400

    alter campaign serializer to reduce data being calculated

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,10 +1,10 @@
 class CampaignSerializer < ActiveModel::Serializer
   attributes :id, :name, :starting_weekday, :starting_month, :starting_day, :starting_age, :starting_year, :characters, :calendar, :theme, :setting, :starting_level, :skillset, :custom_notes, :races, :klasses
 
-  # def characters
-  #   self.object.characters.map do |ch|
-  #     CharacterCampaignSerializer.new(ch)
-  #   end
-  # end
+  def characters
+    self.object.characters.map do |ch|
+      CharacterCampaignSerializer.new(ch)
+    end
+  end
 
 end
