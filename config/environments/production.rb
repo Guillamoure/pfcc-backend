79220bb21e60b6ca18197b3c37869ae566066e4f
Jack Hilscher <jhilscheriv@gmail.com>
2021-07-23 22:19:45 -0400

    added wss url to production environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
+  config.web_socket_server_url = "wss://characterfinder-api.herokuapp.com/cable" 
+
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
